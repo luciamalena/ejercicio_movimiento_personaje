52fcb8550633b79532503bd0fd026314b09677cc
luciamalena <luciamalena20@gmail.com>
2026-04-09 11:00:42 -0300

    Ajustes de movimientos, animación y colisiones

--- jugador/scripts/jugador.gd
+++ jugador/scripts/jugador.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 @export var movimiento_velocidad = 120.0
 @export var desaceleracion = 0.1
+@export var gravedad = 500.0
 
 var movimiento = Vector2()
 func _physics_process(delta: float) -> void:
+	velocity.y += gravedad * delta 
 	movimiento_horizontal()
-	
+	set_animations()
+	flip()
 	move_and_slide()
 
 func movimiento_horizontal():
@@ -16,3 +19,15 @@ func movimiento_horizontal():
 		velocity.x = movimiento * movimiento_velocidad
 	else:
 		velocity.x = move_toward(velocity.x, 0, movimiento_velocidad * desaceleracion )
+
+func set_animations():
+	if velocity.x != 0:
+		$AnimationPlayer.play("movimiento")
+	if velocity.x == 0:
+		$AnimationPlayer.play("idle")
+		
+func flip():
+	if velocity.x > 0.0:
+		scale.x = scale.y * 1
+	if velocity.x < 0.0:
+		scale.x = scale.y * -1
